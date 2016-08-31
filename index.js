d7753375364a694dcc6674b88d8a357514bd8e36
Olivier Coilland <olivier@coilland.fr>
2016-08-31 15:19:47 +0200

    Added script.

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const LOG_LEVEL = 'warn';
+const NPM_REGISTRY_ROOT = 'https://registry.npmjs.org/';
+const GITHUB_REPOSITORY_RE = /github\.com\/(.*?)(\.git)?$/;
+
+const log = require('npmlog');
+log.level = LOG_LEVEL;
+const RegClient = require('npm-registry-client');
+const npmClient = new RegClient({
+    log: log
+});
+const github = require('octonode');
+const githubClient = github.client();
+
+function getRepositoryName(pkg) {
+    return new Promise((resolve, reject) => {
+        npmClient.get(NPM_REGISTRY_ROOT + pkg, {}, (error, data) => {
+            if (error) {
+                reject(error.message);
+                return;
+            }
+
+            const repository = data.repository;
+            if (!repository) {
+                reject('No repository defined in package.json');
+                return;
+            }
+
+            const repositoryUrl = repository.url;
+            const matches = repositoryUrl.match(GITHUB_REPOSITORY_RE);
+            if (!matches) {
+                reject('No GitHub repository found in ' + repositoryUrl);
+                return;
+            }
+
+            const githubRepository = matches[1];
+            resolve(githubRepository);
+        });
+    });
+}
+
+function getRepository(pkg) {
+    return new Promise((resolve, reject) => {
+        getRepositoryName(pkg)
+        .then((name) => {
+            githubClient.repo(name).info((error, data) => {
+                if (error) {
+                    reject(error.message);
+                    return;
+                }
+
+                resolve(data);
+            });
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+module.exports.getRepositoryName = getRepositoryName;
+module.exports.getRepository = getRepository;
