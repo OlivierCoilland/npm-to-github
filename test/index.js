62bf64a3f46398b74ed9dda147b05800aee6377e
Olivier Coilland <olivier@coilland.fr>
2017-04-16 23:24:07 +0200

    Added some tests.

---
+++ test/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const npmToGitHub = require('../index');
+const assert = require('assert');
+
+describe('npm-to-github', function() {
+    it('should return the repository name', function(done) {
+        npmToGitHub.getRepositoryName('npm-to-github')
+        .then((name) => {
+            assert.equal(name, 'OlivierCoilland/npm-to-github');
+            done();
+        })
+        .catch((error) => {
+            done(error);
+        });
+    });
+
+    it('should return the repository', function(done) {
+        npmToGitHub.getRepository('npm-to-github')
+        .then((repository) => {
+            assert.equal(repository.id, 66943137);
+            done();
+        })
+        .catch((error) => {
+            done(error);
+        });
+    });
+
+    it('should fail with a fake repository', function(done) {
+        npmToGitHub.getRepositoryName('fake-repository')
+        .then((name) => {
+            done(new Error('This repository should not exist!'));
+        })
+        .catch((error) => {
+            done();
+        });
+    });
+});
+
+
